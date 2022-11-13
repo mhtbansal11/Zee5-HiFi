e6637219ca0d08dced9ec2441c147ce6c347d753
mhtbansal11 <mhtbansal11@gmail.com>
2022-11-13 18:45:04 +0530

    day-4-API

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
+
 server.use(middlewares);
 server.use(router);
 
